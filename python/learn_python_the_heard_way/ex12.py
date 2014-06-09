d00f5717ba61da5910d8ca15df76c1f49f3bb1d7
Werther <werther0331@gmail.com>
2014-06-09 22:33:54 +0800

    * update ex

---
+++ python/learn_python_the_heard_way/ex12.py
@@ -0,0 +1,10 @@
+#! /bin/python
+
+# new line for python in emacs
+print
+
+age = raw_input("How old are you?")
+height = raw_input("How tall are you?")
+weight = raw_input("How much do you weight?")
+
+print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
