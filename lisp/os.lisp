e03798d823bc991b9a6a7873a38006c8f64f9b4d
pengzhang <pengzhangdev@gmail.com>
2014-06-11 18:04:31 +0800

    * lisp/os.lisp: support multi args in icl-shell commands

--- lisp/os.lisp
+++ lisp/os.lisp
@@ -13,7 +13,7 @@
 (defun icl-update-prompt-info ()
   "update *icl-usrname*  *icl-hostname* and *icl-prompt*"
   (values
-   (setf *icl-usrname* (icl-getenv icl-usrname-key))
+   (setf *icl-usrname* (icl-getenv icl-usorname-key))
    (setf *icl-hostname* (icl-getenv icl-hostname-key))
    (setf *icl-prompt* (if (equal *icl-usrname* "root")
                           #\#
@@ -23,21 +23,21 @@
   "Get environment form OS such as linux/unix"
   (SB-UNIX::posix-getenv name))
 
-(defun icl-shell (progname &optional (args nil))
+(defun icl-shell (progname &rest args)
   (let (retval)
     (setf retval (sb-ext:run-program progname
                                      (if (null args)
                                                   (list)
-                                                  (list args))
+                                                  args)
                                      :output *standard-output*))
     (sb-ext:process-exit-code retval)))
 
 (defun icl-do-directory (pathname func)
   "")
 
-(defun icl-unit-test (program &optional (args nil))
+(defun icl-unit-test (program &rest args)
   (if (null args)
       (format t "args is nil")
-      (format t "args is not nil")))
+      (format t "~A ~%" (list args))))
 
 
