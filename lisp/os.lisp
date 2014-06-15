21d650d966719cc10a11feea23e89e2bd9a10e50
Werther <werther0331@gmail.com>
2014-06-15 15:21:47 +0800

    * [lisp] add feature: auto get env PATH and search command in it to run   with icl-shell

--- lisp/os.lisp
+++ lisp/os.lisp
@@ -35,16 +35,30 @@
 (defun icl-shell (progname &rest args)
   "run shell commands with args.
         if last args is stream , set the program output to stream"
-  (let (proc-obj out-stream prog-args)
-    (setf out-stream (car (reverse args)))
-    (if (streamp out-stream)
-        (setf prog-args (reverse (cdr (reverse args))))
-        (progn
-          (setf out-stream *standard-output*)
-          (setf prog-args args)))
-    (setf proc-obj (sb-ext:run-program progname prog-args
-                                       :output out-stream))
-    (sb-ext:process-exit-code proc-obj)))
+  (block func-shell
+    (let ((out-stream (car (reverse args)))
+          (prog-name progname)
+          proc-obj prog-args)
+      (when (null (position #\/ prog-name))
+        ;;(format t "icl-path ~A" *icl-path*)
+        (setf prog-name (icl-maptest
+                         #'(lambda (name path)
+                             (let ((prog-name (concatenate 'string path "/" name)))
+                               (if (probe-file prog-name)
+                                   prog-name
+                                   nil)))
+                         prog-name *icl-path*)))
+      (when (null prog-name)
+           (format t "Command ~A not found" progname)
+           (return-from func-shell))
+      (if (streamp out-stream)
+          (setf prog-args (reverse (cdr (reverse args))))
+          (progn
+            (setf out-stream *standard-output*)
+            (setf prog-args args)))
+      (setf proc-obj (sb-ext:run-program prog-name prog-args
+                                         :output out-stream))
+      (sb-ext:process-exit-code proc-obj))))
 
 (defun icl-split-string (sep str)
   "split string with split and return a list"
@@ -54,27 +68,41 @@
         ((null pos) (append (list tmp-str) result-list))
       (if (equal pos 0)
           (setf tmp-str (subseq tmp-str (+ 1 pos)))
-        (progn
-          (setf result-list (append
-                             (list (subseq tmp-str 0 pos))
-                             result-list))
-          (setf tmp-str (subseq tmp-str (+ 1 pos))))))))
+          (progn
+            (setf result-list (append
+                               (list (subseq tmp-str 0 pos))
+                               result-list))
+            (setf tmp-str (subseq tmp-str (+ 1 pos))))))))
 
 
-(defun icl-do-directory (pathname func)
-  "")
+(defun icl-maptest (func str lst)
+  "operates on successive elements of the lists.
+return first non-nil result"
+  (block icl-maptest-stat
+    (when (null lst)
+        (format t "lst is not found~%")
+        (return-from icl-maptest-stat))
+    (let (ret)
+      (setf ret (funcall func str (car lst)))
+      (if (null ret)
+          (icl-maptest func str (cdr lst))
+          ret))))
 
-
-;;;; test 
+;;;; test
 (defun icl-unit-test (program &rest args)
   (if (null args)
       (format t "args is nil")
       (format t "~A ~%" (reverse args)))
-  (let (out-stream prog-args)
+  (let (prog-name out-stream prog-args)
     (setf out-stream (car (reverse args)))
-    (if (streamp out-stream)
-        (setf prog-args (reverse (cdr (reverse args))))
-        (progn
-          (setf out-stream *standard-output*)
-          (setf prog-args args)))
-    (format t "~A ~A ~%" out-stream prog-args)))
+    (setf prog-name program)
+    (when (null (position #\/ prog-name))
+      (format t "position nil")
+      (setf prog-name (icl-maptest
+                       #'(lambda (name path)
+                           (let ((prog-name (concatenate 'string path "/" name)))
+                             (if (probe-file prog-name)
+                                 prog-name
+                                 nil)))
+                       prog-name *icl-path*)))
+    ))
