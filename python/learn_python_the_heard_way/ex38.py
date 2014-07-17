e648ccd243fd015694adedc159a27d6a407d5a2b
Werther <werther0331@gmail.com>
2014-07-17 22:12:23 +0800

    * [python] update learn python the hard way

---
+++ python/learn_python_the_heard_way/ex38.py
@@ -0,0 +1,27 @@
+#! /usr/bin/python
+
+ten_things = "Apples Oranges Crows Telephone Light Sugar"
+
+print "Wait there's nto 10 things in that list, let's fix that"
+
+stuff = ten_things.split(' ')
+more_stuff = ["Day", "Night", "Song", "Fresbee", "Corn", "Banana", "Girl", "Boy"]
+
+while len(stuff) != 10:
+    next_one = more_stuff.pop()
+    print "Adding: ", next_one
+    stuff.append(next_one)
+
+    print "There's %d items now." % len(stuff)
+
+print "There we go:", stuff
+
+print "Let's do some things with stuff."
+
+print stuff[1]
+print stuff[-1]
+print stuff.pop()
+print ' '.join(stuff)
+print '#'.join(stuff[3:5])
+
+
