bb70a746be0eb409d7b744339f338265cdd95da7
Werther <werther0331@gmail.com>
2015-11-07 21:44:10 +0800

    add eventloop.py and shell.py

---
+++ python/virtualp2p/shell.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+#
+# shell.py --- 
+# 
+# Filename: shell.py
+# Description: shell model to parse input args and print outputs
+# Author: Werther Zhang
+# Maintainer: zzd
+# Created: Sat Nov  7 17:38:19 2015 (+0800)
+# 
+
+# Change Log:
+# 
+# 
+
