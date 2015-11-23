c1db90943e68006b084cfe48ea417ad1a172c5b1
wertherzhang <wertherzhang@china-liantong.com>
2015-11-23 16:06:26 +0800

    add sudoku.py

---
+++ python/sudoku/sudoku.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+#
+# sudoku.py ---
+#
+# Filename: sudoku.py
+# Description:
+# Author: Werther Zhang
+# Maintainer:
+# Created: Mon Nov 23 16:05:58 2015 (+0800)
+#
+
+# Change Log:
+#
+#
+
