53e99c8655271b04a1149297f70afd4c453fe34c
wertherzhang <wertherzhang@china-liantong.com>
2016-06-01 12:40:45 +0800

    add docker script and android/cos build container

---
+++ docker/docker.md
@@ -0,0 +1,11 @@
++ docker backup
+
+`
+# docker save -o ~/docker/cos.tar liantong/cos
+`
+
++ docker restore
+
+`
+# docker load -i ~/cos.tar
+`
---
+++ docker/docker_setup.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+#
+# docker_setup.sh ---
+#
+# Filename: docker_setup.sh
+# Description:
+# Author: Werther Zhang
+# Maintainer:
+# Created: 二  5月 31 10:58:43 2016 (+0800)
+#
+
+# Change Log:
+#
+#
+
+sudo docker build -t liantong/cos .
